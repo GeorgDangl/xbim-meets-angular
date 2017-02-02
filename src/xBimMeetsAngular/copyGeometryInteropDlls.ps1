5f92343c1e4bc3837324249121bab6e01fd3656c
Georg Dangl <dangl@iabi.eu>
2017-02-02 15:42:35 +0100

    Update script to copy Xbim.Geometry interop dlls

--- src/xBimMeetsAngular/copyGeometryInteropDlls.ps1
+++ src/xBimMeetsAngular/copyGeometryInteropDlls.ps1
@@ -1,5 +1,8 @@
 $xBimGeometryPackages = Join-Path -Path $env:USERPROFILE -ChildPath "\.nuget\packages\Xbim.Geometry"
 $latestxBimGeometryPackage32 = Join-Path -Path ((Get-ChildItem -Path $xBimGeometryPackages | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "build\x86\Xbim.Geometry.Engine32.dll"
 $latestxBimGeometryPackage64 = Join-Path -Path ((Get-ChildItem -Path $xBimGeometryPackages | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "build\x64\Xbim.Geometry.Engine64.dll"
-Copy-Item -Path $latestxBimGeometryPackage32 -Destination "Dependencies"
-Copy-Item -Path $latestxBimGeometryPackage64 -Destination "Dependencies"
+if (!(Test-Path "$PSScriptRoot\Dependencies")){
+    New-Item -ItemType Directory -Path "$PSScriptRoot\Dependencies"
+}
+Copy-Item -Path $latestxBimGeometryPackage32 -Destination "$PSScriptRoot\Dependencies\Xbim.Geometry.Engine32.dll"
+Copy-Item -Path $latestxBimGeometryPackage64 -Destination "$PSScriptRoot\Dependencies\Xbim.Geometry.Engine64.dll"
